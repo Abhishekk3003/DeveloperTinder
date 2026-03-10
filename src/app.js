482a40f98f6b15bc78149063af4169d0bb413dcd
Abhishekk3003 <abhishekrawat3062@gmail.com>
2026-03-10 14:36:12 +0530

    exploring the HTTP methods

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ const express = require("express");
 const app = express();
 
 
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
     res.send("hello world");
 })
-app.use("/test2",(req,res) => {
+app.post("/test2",(req,res) => {
     res.send("hello world 2");
 })
 
+app.delete("/test3",(req,res) => {
+    res.send("deleted succcessfully");
+})
 
 app.listen(7000,() =>{
     console.log("server is running on port 7000");
